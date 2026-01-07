0f64a8281e826a4173bf2f2d9291fe89b979e07c
Rodrigo Almeida Dourado <115838474+Rodrigo-Dourad0@users.noreply.github.com>
2026-01-07 00:27:23 -0300

    refatora AuthContext

--- projeto-web-react/src/context/AuthContext.jsx
+++ projeto-web-react/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
-
+import api from '../shared/services/api'; //
 
 export const AuthContext = createContext();
 
@@ -7,7 +7,7 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    //  Ao carregar a página, verifica se já tem token salvo
+   
     useEffect(() => {
         const recoverUser = () => {
             const storedToken = localStorage.getItem('token');
@@ -15,6 +15,7 @@ export const AuthProvider = ({ children }) => {
 
             if (storedToken && storedUser) {
                 setUser(JSON.parse(storedUser));
+
             }
             setLoading(false);
         };
@@ -25,23 +26,11 @@ export const AuthProvider = ({ children }) => {
     // Função de Login 
     const login = async (email, senha) => {
         try {
-            
-            const response = await fetch('http://localhost:8080/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email, senha }),
-            });
+    
+            const response = await api.post('/login', { email, senha });
 
-            if (!response.ok) {
-                throw new Error('Erro na autenticação');
-            }
+            const data = response.data; 
 
-            const data = await response.json();
-
-            
-            // Se o token vier solto ou em outro formato, ajusta
             const loggedUser = {
                 token: data.token,
                 email: email,
@@ -51,11 +40,11 @@ export const AuthProvider = ({ children }) => {
             localStorage.setItem('user', JSON.stringify(loggedUser));
 
             setUser(loggedUser);
-            return true; // Login sucesso
+            return true; // Login com sucesso
 
         } catch (error) {
             console.error("Erro ao logar:", error);
-            return false; // Login falhou
+            return false; // Falha no login
         }
     };
 
